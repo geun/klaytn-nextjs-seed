254443f80645d8a46b558e0e053a517074afa568
Ken Lee <ken@flui.ai>
2019-09-03 19:51:09 +0900

    refactor safeSetCurrentBalance

--- pages/account.js
+++ pages/account.js
@@ -1,39 +1,52 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import dynamic from 'next/dynamic';
 import { useWeb3Context } from 'web3-react';
 
 import { ethers } from 'ethers';
 
-function getCurrentBalance(account) {
-	let provider = new ethers.providers.Web3Provider(web3.currentProvider);
+const setCurrentHandle = (context, setHandler) => e => {
+	e.preventDefault();
+	return updateBalance(context, setHandler);
+};
 
-	return async e => {
-		e.preventDefault();
+const updateBalance = async (context, setCurrentBalance) => {
+	if (!context.active) return;
+	const { account, library } = context;
+	const current = await library.getBalance(account);
+	setCurrentBalance(current.toString());
+};
 
-		const currentBalance = await provider.getBalance(account);
-		console.log('account: ', account);
-		console.log('currentBalance', currentBalance.toString());
-	};
-}
-
+const initialEther = ethers.utils.parseEther('0');
 // This component must be a child of <App> to have access to the appropriate context
 function Account(props) {
+	if (ethereum && !!ethereum.autoRefreshOnNetworkChange) {
+		ethereum.autoRefreshOnNetworkChange = false;
+	}
+
 	const context = useWeb3Context();
-	// const [account, setAccount] = useState();
+	const mountedRef = useRef(false);
 
-	console.log('context', context);
-	// ethereum.autoRefreshOnNetworkChange = false;
+	useEffect(() => {
+		mountedRef.current = true;
+		return () => (mountedRef.current = false);
+	});
 
-	console.log('ethereum.isMetaMask', ethereum.isMetaMask);
+	// const manager = useWeb3Manager();
+	const [currentBalance, setCurrentBalance] = useState(initialEther);
+
+	const safeSetCurrentBalance = (...args) => mountedRef.current && setCurrentBalance(...args);
+
+	useEffect(() => {
+		console.log('context changed', context);
+		updateBalance(context, safeSetCurrentBalance);
+	}, [context]);
+
 	useEffect(() => {
 		console.log('setFirstValidConnector');
 		// context.setFirstValidConnector(['MetaMask', 'Infura']);
 		context.setFirstValidConnector(['MetaMask']);
-		// ethereum.enable().then(account => setAccount(account));
 	}, []);
-
-	console.log('check context');
 
 	if (!context.active && !context.error) {
 		// loading
@@ -42,14 +55,16 @@ function Account(props) {
 		//error
 		return (
 			<>
-				<h1>error</h1>
+				<h1>Error</h1>
 				<pre>{JSON.stringify(context.error)}</pre>
 			</>
 		);
 	} else {
 		// success
 		const { active, connectorName, account, networkId } = context;
-		// console.log(ethereum.selectedAddres());
+		// balance is a BigNumber (in wei); format is as a sting (in ether)
+		let etherString = ethers.utils.formatEther(currentBalance);
+
 		return (
 			active && (
 				<>
@@ -57,8 +72,10 @@ function Account(props) {
 					<p>Active Connector: {connectorName}</p>
 					<p>Account: {account || 'None'}</p>
 					<p>Network ID: {networkId}</p>
-
-					<button onClick={getCurrentBalance(account)}>Get Current Balance</button>
+					<p>Current Balance: {etherString}</p>
+					<button onClick={setCurrentHandle(context, safeSetCurrentBalance)}>
+						Get Current Balance
+					</button>
 				</>
 			)
 		);
