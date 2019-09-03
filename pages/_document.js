7e543a6cfe5d79d170a2f3ed6025390c3d608044
Ken Lee <ken@flui.ai>
2019-09-03 20:55:42 +0900

    fix: styled component server-side-rendering

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,29 @@
-// _document is only rendered on the server side and not on the client side
-// Event handlers like onClick can't be added to this file
+import Document from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-// ./pages/_document.js
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-
-class MyDocument extends Document {
+export default class MyDocument extends Document {
 	static async getInitialProps(ctx) {
-		const initialProps = await Document.getInitialProps(ctx);
-		return { ...initialProps };
-	}
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
 
-	render() {
-		return (
-			<Html>
-				<Head />
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+				});
+
+			const initialProps = await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				)
+			};
+		} finally {
+			sheet.seal();
+		}
 	}
 }
-
-export default MyDocument;
